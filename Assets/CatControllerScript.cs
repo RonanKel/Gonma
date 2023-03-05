692926160cbe08c91ba837c99574b4247aaf07aa
RonanKel <ronandugankelly@gmail.com>
2023-03-04 17:04:03 -0800

    Basic Fishing Mechanics File Fixes
    Made sure that files were distinct and operable.

--- Assets/CatControllerScript.cs
+++ Assets/CatControllerScript.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class CatControllerScript : MonoBehaviour
 {
     private bool casting = false;
     private bool fishBite = false;
     private float fishTimer = 0f;
     private float catchWarningTime;
-    private bool caught = false;
     private bool alerted = false;
     private float alertTimer = 0f;
 
@@ -26,63 +25,73 @@ public class CharacterController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         // casts
-        if (Input.GetKeyDown(KeyCode.Space) && !casting) {
+        if (Input.GetKeyDown(KeyCode.Space) && !casting)
+        {
             cast();
         }
         // if its clicked again it reels it in
-        else if (Input.GetKeyDown(KeyCode.Space) && casting) {
+        else if (Input.GetKeyDown(KeyCode.Space) && casting)
+        {
             //if there's a fish, it's caught
-            if (fishTimer < catchWarningTime+reactionTime && fishBite) {
+            if (fishTimer < catchWarningTime + reactionTime && fishBite)
+            {
                 Debug.Log("Caught!");
             }
             uncast();
         }
         // if the timer goes to long the fish goes away
-        if (fishTimer > catchWarningTime+reactionTime) {
+        if (fishTimer > catchWarningTime + reactionTime)
+        {
             fishBite = false;
             Debug.Log("U suc");
         }
-        
+
 
         // casting timer
-        if (casting) {
+        if (casting)
+        {
             fishTimer += Time.deltaTime;
             Debug.Log(fishTimer);
         }
         // alert timer
-        if (alerted) {
+        if (alerted)
+        {
             alertTimer += Time.deltaTime;
         }
 
 
         // handles alert timer and deactivation
-        if (fishTimer > catchWarningTime && !alerted) {
+        if (fishTimer > catchWarningTime && !alerted)
+        {
             alert.SetActive(true);
             fishBite = true;
             alerted = true;
         }
-        if (alertTimer > alertLength) {
+        if (alertTimer > alertLength)
+        {
             alert.SetActive(false);
         }
     }
 
-    void cast() {
+    void cast()
+    {
         Debug.Log("Casting");
         casting = true;
         alertTimer = 0f;
         alerted = false;
         hook.SetActive(true);
-        catchWarningTime = initDelayTime + Random.Range(1f,5f);
+        catchWarningTime = initDelayTime + Random.Range(1f, 5f);
     }
 
-    void uncast() {
+    void uncast()
+    {
         Debug.Log("Uncasting");
         fishTimer = 0f;
         fishBite = false;
