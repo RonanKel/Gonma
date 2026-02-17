fdefbd5567127353fec09b4f436eb5a77fce39e0
Wagical <ollenbrookj@gmail.com>
2026-02-16 20:49:07 -0800

    Update Fix and Clean up

--- Assets/Scripts/Masher.cs
+++ Assets/Scripts/Masher.cs
@@ -9,12 +9,8 @@ public class Masher : MonoBehaviour
 [SerializeField] GameObject MasherCircle;
 [SerializeField] GameObject IndicatorCircle;
 [SerializeField] float StartingScale;
-// [SerializeField] GameObject EvilCircle;
-// [SerializeField] GameObject Fail;
-// [SerializeField] GameObject Win;
-// [SerializeField] int mash;
+
 private Vector3 posChange, negChange;
-// private bool result = false;
 
 [HideInInspector] public bool done = false;
 [HideInInspector] public bool result;
@@ -22,39 +18,31 @@ private Vector3 posChange, negChange;
     void Start()
     {
         
-    
+    }
+
+    // FixedUpdate is called once per REAL frame
+    void FixedUpdate()
+    {
+        ColorHeat();
+        BasicMash();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ColorHeat();
-        BasicMash();
-        // switch (mash){
-        //     case 0:
-        //         BasicMash();
-        //         break;
-        //     case 1:
-        //         EvilMash();
-        //         break;
-        //     deafult:
-        //         break;
-        // }
+        if (!done){
+            OnMouseOver(0.1f);
+        }
     }
 
 
     void ColorHeat() {
         Color heat = new Color(1f, 0.5f, 0f, 1f);
         // IndicatorCircle.material = heat;
-        //  75%> green
-        //  75 - 50 yellow
-        // 50 25 orange
+        // 75%> green
+        // 75 - 50 yellow
+        // 50 - 25 orange
         // 25 red
-        // float Gcolor = IndicatorCircle.transform.localScale.x*255;
-        // float Rcolor = 127 - Gcolor;
-        // Debug.Log("Red: " + Rcolor );
-        // Debug.Log("Green: " + Gcolor );
-        // Color heat = new Color(Rcolor,Gcolor,0,IndicatorCircle.transform.localScale.x);
         if (IndicatorCircle.transform.localScale.x >= .75f)
         {
             heat = Color.green;
@@ -73,8 +61,7 @@ private Vector3 posChange, negChange;
 
     void BasicMash() {
         if (!done){
-            OnMouseOver(0.1f);
-            Decay(-0.0005f);
+            Decay(-0.005f);
             // I know this is dumb
             if (IndicatorCircle.transform.localScale.x < Vector3.zero.x){
                 lose();
@@ -86,47 +73,14 @@ private Vector3 posChange, negChange;
 
     }
 
-    // void EvilMash() {
-    //     if (!result){
-    //         OnMouseOver(0.1f);
-    //         // Decay(-0.0005f);
-    //         GrowingEvil(0.0005f);
-    //         // I know this is dumb
-    //         if (IndicatorCircle.transform.localScale.x < EvilCircle.transform.localScale.x || EvilCircle.transform.localScale.x > Vector3.one.x){
-    //             lose();
-    //         }
-    //         if (IndicatorCircle.transform.localScale.x > Vector3.one.x){
-    //             win();
-    //         }
-    //         }
-
-    // }
-
-    // Next one Q W E inputs 
-//     if (Input.GetKeyDown(KeyCode.A))
-// {
-//     //weapon stuff here
-// }
-
-
-
-
-    // void GrowingEvil(float gain){
-    //     posChange = new Vector3(gain, gain, gain);
-    //     EvilCircle.transform.localScale += posChange;
-    // }
 
 
     void win() {
-        // Win.SetActive(true);
-        // Fail.SetActive(false);
         done = true;
         result = true;
     }
 
     void lose() {
-        // Win.SetActive(false);
-        // Fail.SetActive(true);
         done = true;
         result = false;
     }
