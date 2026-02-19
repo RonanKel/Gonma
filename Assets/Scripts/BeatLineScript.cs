9afe4484b3aa21398759f333702c01f50585139c
Wagical <ollenbrookj@gmail.com>
2026-02-19 01:44:26 -0800

    First bit of combo

--- Assets/Scripts/BeatLineScript.cs
+++ Assets/Scripts/BeatLineScript.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Text.RegularExpressions;
 public class BeatLineScript : MonoBehaviour
 {
     [SerializeField] LayerMask noteMask;
     [SerializeField] string inputKey;
 
     [SerializeField] TextMeshProUGUI statusText;
+    [SerializeField] TextMeshProUGUI comboText;
 
     [SerializeField] float poorLength = 1.5f;
     [SerializeField] float niceLength = 1f;
@@ -19,6 +21,7 @@ public class BeatLineScript : MonoBehaviour
 
     private MusicManagerScript mmScript;
 
+    private int i = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -39,10 +42,10 @@ public class BeatLineScript : MonoBehaviour
             statusText.color = new Color(0, 1, 0, 1f);
             statusText.CrossFadeAlpha(0, .5f, false);
             statusText.text = "Perfect!";
-
+            comboFun(3);
             Destroy(perfect.transform.gameObject);
             mmScript.score += 3;
-            Debug.Log("Perfect!");
+            // Debug.Log("Perfect!");
         }
         else if (Input.GetKeyDown(inputKey) && nice) {
             // FADE IN .1 YELLOW, FADE OUT .5
@@ -50,10 +53,10 @@ public class BeatLineScript : MonoBehaviour
             statusText.color = new Color(1, 1, 0, 1f);
             statusText.CrossFadeAlpha(0, .5f, false);
             statusText.text = "Nice!";
-            
+            comboFun(2);
             Destroy(nice.transform.gameObject);
             mmScript.score += 2;
-            Debug.Log("Nice!");
+            // Debug.Log("Nice!");
         }
         else if (Input.GetKeyDown(inputKey) && poor) {
             // FADE IN .1 ORANGE, FADE OUT .5
@@ -61,10 +64,10 @@ public class BeatLineScript : MonoBehaviour
             statusText.color = new Color(1, .64f, 0, 1f);
             statusText.CrossFadeAlpha(0, .5f, false);
             statusText.text = "Poor!";
-
+            comboFun(1);
             Destroy(poor.transform.gameObject);
             mmScript.score++;
-            Debug.Log("Poor!");
+            // Debug.Log("Poor!");
         }
         else if (Input.GetKeyDown(inputKey)){
             // FADE IN .1 RED, FADE OUT .5
@@ -72,9 +75,9 @@ public class BeatLineScript : MonoBehaviour
             statusText.color = new Color(1, 0, 0);
             statusText.CrossFadeAlpha(0, .5f, false);
             statusText.text = "Miss!";
-
+            comboFun(0);
             mmScript.score -= 1;
-            Debug.Log("Miss!");
+            // Debug.Log("Miss!");
         }
         
     }
@@ -94,14 +97,25 @@ public class BeatLineScript : MonoBehaviour
     }
 
 
-// void TextFade()
-//     {
-//         while (statusText.color.a > 0.0f)
-//         {
-//             statusText.color = new Color(statusText.color.r, statusText.color.g, statusText.color.b, statusText.color.a - (Time.deltaTime / 1f));
+void comboFun(int score)
+    {
+        if (score != 0){
+            // Trin wreck of parseing a int of the text box
+            var matches = Regex.Matches(comboText.text, @"\d+");
+            string st2 ="";
+            foreach(var match in matches){
+                st2 += match;
+            }
+            i = int.Parse(st2);
+            // Debug.Log("" + i);
+            ++i;
+            comboText.text = "Combo: " + i;
 
-//         }
-//     }  
+        }else{
+            i = 0;
+            comboText.text = "Combo: " + i;
+        }
+    }  
 
 
 
