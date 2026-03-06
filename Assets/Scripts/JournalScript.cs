0ab3f5c931816c024df2258d5b7a08cc74a6a74d
RonanKel <ronandugankelly@gmail.com>
2026-03-05 19:36:22 -0800

    Added different pages to journals based on fish levels

--- Assets/Scripts/JournalScript.cs
+++ Assets/Scripts/JournalScript.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
 
 public class JournalScript : MonoBehaviour
 {
@@ -17,55 +19,114 @@ public class JournalScript : MonoBehaviour
     [SerializeField] GameObject extra1Text;
     [SerializeField] GameObject extra2Text;
 
+    private int curr_page = 0;
+    [SerializeField] List<Level> pages;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SetJournalObjects(pages[curr_page]);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            FlipPageBackward();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            FlipPageForward();
+        }
+    }
+
+    public void FlipPageForward()
+    {
+        if (curr_page < pages.Count - 1) {
+            curr_page++;
+            SetJournalObjects(pages[curr_page]);
+        }
+    }
+
+    public void FlipPageBackward()
+    {
+        if (curr_page > 0) {
+            curr_page--;
+            SetJournalObjects(pages[curr_page]);
+        }
     }
 
     void OnEnable()
     {
-        SetJournalObjectsActive(level_name);
+        SetJournalObjects(pages[curr_page]);
     }
 
-    void SetJournalObjectsActive(string level_name)
+    void SetJournalObjects(Level level)
     {
+        SetJournalObjectsDeactive();
+        string level_name = level.name;
         if (PlayerPrefs.HasKey(level_name))
         {
             scoreText.SetActive(true);
             scoreText.GetComponent<TextMeshProUGUI>().text = PlayerPrefs.GetInt(level_name).ToString();
+           
             nameText.SetActive(true);
+            nameText.GetComponent<TextMeshProUGUI>().text = level.fishname;
+            
             scientificNameText.SetActive(true);
+            scientificNameText.GetComponent<TextMeshProUGUI>().text = level.scientificFishname;
+            
             fishImage.SetActive(true);
+            fishImage.GetComponent<Image>().sprite = level.fishSprite;
+            
             meetingThoughtsText.SetActive(true);
+            meetingThoughtsText.GetComponent<TextMeshProUGUI>().text = level.meetingThoughts;
         }
         if (PlayerPrefs.HasKey(level_name + "award1")) {
             if (PlayerPrefs.GetInt(level_name + "award1") == 1)
             {
                 descriptionText.SetActive(true);
+                descriptionText.GetComponent<TextMeshProUGUI>().text = level.description;
                 award1Image.SetActive(true);
+                award1Image.GetComponent<Image>().sprite = level.award1;
                 trinketImage.SetActive(true);
+                trinketImage.GetComponent<Image>().sprite = level.trinketSprite;
             }
         }
         if (PlayerPrefs.HasKey(level_name + "award2")) {
             if (PlayerPrefs.GetInt(level_name + "award2") == 1)
             {
                 award2Image.SetActive(true);
+                award2Image.GetComponent<Image>().sprite = level.award2;
                 extra1Text.SetActive(true);
+                extra1Text.GetComponent<TextMeshProUGUI>().text = level.extra1;
             }
         }
         if (PlayerPrefs.HasKey(level_name + "award3")) {
             if (PlayerPrefs.GetInt(level_name + "award3") == 1)
             {
                 award3Image.SetActive(true);
+                award3Image.GetComponent<Image>().sprite = level.award3;
                 extra2Text.SetActive(true);
+                extra2Text.GetComponent<TextMeshProUGUI>().text = level.extra2;
             }
         }
+    }
+
+    void SetJournalObjectsDeactive()
+    {
+        nameText.SetActive(false);
+        scientificNameText.SetActive(false);
+        meetingThoughtsText.SetActive(false);
+        fishImage.SetActive(false);
+        descriptionText.SetActive(false);
+        scoreText.SetActive(false);
+        award1Image.SetActive(false);
+        award2Image.SetActive(false);
+        award3Image.SetActive(false);
+        trinketImage.SetActive(false);
+        extra1Text.SetActive(false);
+        extra2Text.SetActive(false);
     }
 }
--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -5,16 +5,42 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Levels", menuName = "Level")]
 public class Level : ScriptableObject
 {
+
+    // Song info
     public new string name;
 
     public Sprite fishSprite;
 
     public AudioClip song;
 
-    public Sprite trinketSprite;
-
     public string GetLevelData () {
         return "LevelData/" + name + ".txt";
     }
+
+
+    // Journal info
+    public new string fishname;
+
+    public new string scientificFishname;
+
+    public new string meetingThoughts;
+
+    public Sprite journalFishSprite;
+
+    public new string description;
+
+    public Sprite trinketSprite;
+
+    public Sprite award1;
+
+    public Sprite award2;
+
+    public Sprite award3;
+
+    public new string extra1;
+
+    public new string extra2;
+
+    
 
 }
