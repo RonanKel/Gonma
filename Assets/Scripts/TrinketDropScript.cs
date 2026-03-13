582eddddea40707df4f421912ac5730edd001a68
RonanKel <ronandugankelly@gmail.com>
2026-03-13 15:19:58 -0700

    Added info pop up to bait

--- Assets/Scripts/TrinketDropScript.cs
+++ Assets/Scripts/TrinketDropScript.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.Events;
 
-public class TrinketDropScript : MonoBehaviour, IDropHandler, IPointerDownHandler
+public class TrinketDropScript : MonoBehaviour, IDropHandler, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
 {   
     [SerializeField] Sprite bkgdSprite;
     private Image image;
@@ -13,14 +13,32 @@ public class TrinketDropScript : MonoBehaviour, IDropHandler, IPointerDownHandle
     public UnityEvent<Level> trinketAdded = new UnityEvent<Level>();
     public UnityEvent<Level> trinketRemoved = new UnityEvent<Level>();
 
+    public UnityEvent<Level, Vector3> trinketHoverStart = new UnityEvent<Level, Vector3>();
+    public UnityEvent trinketHoverEnd = new UnityEvent();
+
 
     private void Start()
     {
         image = GetComponent<Image>();
     }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (level != null) {
+            trinketHoverStart.Invoke(level, transform.position);
+        }
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        trinketHoverEnd.Invoke();
+    }
+
+
     public void OnPointerDown(PointerEventData eventData)
     {
+        trinketHoverEnd.Invoke();
         if (level != null)
         {
             trinketRemoved.Invoke(level);
